e03467a3f8252b5676084130878d2982d3812884
Joshua Carey <jdcarey128@gmail.com>
2020-11-07 13:25:44 -0700

    Destroy dependent item_discounts

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,7 +1,7 @@
 class Discount < ApplicationRecord
   validates_presence_of :discount_percent
   validates_presence_of :item_threshold
-  has_many :item_discounts
+  has_many :item_discounts, dependent: :destroy
   has_many :items, through: :item_discounts
   validates_numericality_of :discount_percent, greater_than: 0, less_than_or_equal_to: 100
   validates_numericality_of :item_threshold, greater_than: 0, less_than_or_equal_to: 100
