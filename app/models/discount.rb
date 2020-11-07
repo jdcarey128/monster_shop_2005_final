2b9a1013d67a98dfca735dff22088dc07914e9e8
Joshua Carey <jdcarey128@gmail.com>
2020-11-07 09:38:53 -0700

    Add apply_to_all_merchant_items method for discount

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,4 +3,10 @@ class Discount < ApplicationRecord
   validates_presence_of :item_threshold
   has_many :item_discounts
   has_many :items, through: :item_discounts
+
+  def apply_to_all_merchant_items(merchant_id)
+    self.items << Merchant.find_by_id(merchant_id).items
+  end
+
+
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -10,4 +10,41 @@ RSpec.describe Discount, type: :model do
     it {should have_many :item_discounts}
     it {should have_many(:items).through(:item_discounts)}
   end
+
+  describe 'instance methods' do
+    describe '#apply_to_all_merchant_items()' do
+      it "creates an item association for all merchant items" do
+        merchant = create(:merchant)
+        merchant_employee = create(:user, role: 1, merchant: merchant)
+        item_1 = create(:item, merchant: merchant)
+        item_2 = create(:item, merchant: merchant)
+        item_3 = create(:item, merchant: merchant)
+        discount = create(:discount)
+
+        discount.apply_to_all_merchant_items(merchant.id)
+        merchant.reload
+
+        items = [item_1, item_2, item_3]
+
+        expect(discount.items).to eq(items)
+      end
+
+      it "creates an association with only one merchant's items" do
+        merchant = create(:merchant)
+        merchant_2 = create(:merchant)
+        merchant_employee = create(:user, role: 1, merchant: merchant)
+        item_1 = create(:item, merchant: merchant)
+        item_2 = create(:item, merchant: merchant)
+        item_3 = create(:item, merchant: merchant_2)
+        discount = create(:discount)
+
+        discount.apply_to_all_merchant_items(merchant_2.id)
+        merchant_2.reload
+
+        items = [item_3]
+
+        expect(discount.items).to eq(items)
+      end
+    end
+  end
 end
